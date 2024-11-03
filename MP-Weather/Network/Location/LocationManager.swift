de462ba27d3c712b3a0051bbc114206ab740c37a
Nicolas Dedual <nick@dedual.com>
2024-11-03 16:36:15 -0500

    Update LocationManager.swift
    Fixes a task leak issue found

--- MP-Weather/Network/Location/LocationManager.swift
+++ MP-Weather/Network/Location/LocationManager.swift
@@ -10,14 +10,14 @@ import CoreLocation
 class LocationManager: NSObject, CLLocationManagerDelegate {
     
     private let locationManager = CLLocationManager()
-    private var continuation: CheckedContinuation<CLLocation, Error>?
+    private var nillableContinuation: CheckedContinuation<CLLocation, Error>?
     
     static var authStatus:Bool = false
     
     var currentLocation:CLLocation {
         get async throws {
             return try await withCheckedThrowingContinuation{ continuation in
-                self.continuation = continuation
+                self.nillableContinuation = continuation
                 locationManager.requestLocation()
             }
         }
@@ -33,17 +33,23 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
         switch locationManager.authorizationStatus {
         case .notDetermined:
             locationManager.requestWhenInUseAuthorization()
-            continuation?.resume(throwing: LocationError.awaitingPermission)
-            continuation = nil
-            LocationManager.authStatus = false
+            if let continuation = nillableContinuation {
+                nillableContinuation = nil
+                continuation.resume(throwing: LocationError.awaitingPermission)
+                LocationManager.authStatus = false
+            }
         case .denied:
-            continuation?.resume(throwing: LocationError.denied("We are unable to retrieve your device's location via Location Services.\nPlease authorize."))
-            continuation = nil
-            LocationManager.authStatus = false
+            if let continuation = nillableContinuation {
+                nillableContinuation = nil
+                continuation.resume(throwing: LocationError.denied("We are unable to retrieve your device's location via Location Services.\nPlease authorize."))
+                LocationManager.authStatus = false
+            }
         case .restricted:
-            continuation?.resume(throwing: LocationError.denied("We are unable to retrieve your device's location via Location Services.\nPlease authorize."))
-            continuation = nil
-            LocationManager.authStatus = false
+            if let continuation = nillableContinuation {
+                nillableContinuation = nil
+                continuation.resume(throwing: LocationError.denied("We are unable to retrieve your device's location via Location Services.\nPlease authorize."))
+                LocationManager.authStatus = false
+            }
         default:
             LocationManager.authStatus = true
             return
@@ -51,15 +57,18 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-            if let lastLocation = locations.last {
-                continuation?.resume(returning: lastLocation)
-                continuation = nil
+        if let lastLocation = locations.last {
+            if let continuation = nillableContinuation {
+                    nillableContinuation = nil
+                    continuation.resume(returning: lastLocation)
             }
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        
-        continuation?.resume(throwing: LocationError.unknown(error.localizedDescription))
-        continuation = nil
+        if let continuation = nillableContinuation {
+            continuation.resume(throwing: LocationError.unknown(error.localizedDescription))
+            nillableContinuation = nil
+        }
     }
 }
